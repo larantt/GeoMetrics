d3e76a8833177a01b988f4dca64df342ca89447d
laratt <laratt@umich.edu>
2024-07-08 17:19:31 +0100

    further additions to plotting and fitting

--- geometrics/plot_tools.py
+++ geometrics/plot_tools.py
@@ -22,13 +22,42 @@ import matplotlib.pyplot as plt         # for creating figures
 import cartopy.crs as ccrs              # for creating geospatially referenced figures
 import cartopy.feature as cfeature      # interface to cartopy.feature
 import scipy
+from matplotlib.lines import Line2D     # for spoofing legends
 
 #mpl.style.use('geometrics_stylesheet.mplstyle')
+
+#############
+## GLOBALS ##
+#############
+
+# Tol color pallete
+lgrey = "#d3d3d3"
+indigo = "#332288"
+grass = "#117733"
+teal = "#44AA99"
+sky = "#88CCEE"
+mustard = "#DDCC77"
+blush = "#CC6677"
+fuscia = "#AA4499"
+mauve = "#882255"
+
+# IBM color pallete
+ibm_blue = "#648FFF"
+ibm_purple = "#785EF0"
+ibm_pink = "#DC267F"
+ibm_oragne = "#FE6100"
+ibm_yellow = "#FFB000"
+
+# Nice printing in the terminal
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+END = '\033[0m'
 
 # FOR DEVELOPER REFERENCE - helpful matplotlib stuff
 # mpl.colors, mpl.patches, mpl.Line2D, mpl.colors.ListedColormap, mpl.collections.LineCollection, mpl.cm, mpl.colors.Normalize
 
-def plot_histogram(data,bins='sqrt',comp_distribution=None,ax=None,labels=[]):
+def plot_histogram(data,bins='sqrt',comp_distribution=None,ax=None,labels=None,ks_alpha=0.05):
     """
     Function to generate a nice histogram of n sets of 
     data. Can be used to compare multiple datasets or to
@@ -56,9 +85,16 @@ def plot_histogram(data,bins='sqrt',comp_distribution=None,ax=None,labels=[]):
     labels : list
         list of string labels to use in histogram plot
 
+    ks_alpha : float
+        significance level to use for the Kolmogorov-Smirnov test
+        if a comparative distribution is supplied
+
     Returns
     -------
     """
+    # set labels to empty list if unspecified
+    if not labels:
+        labels = []
     # check if axes are defined in function already (maybe move this later to save time)
     if not ax:
         # if axis not defined, create a figure and axis for plot
@@ -75,7 +111,7 @@ def plot_histogram(data,bins='sqrt',comp_distribution=None,ax=None,labels=[]):
     
     # define number of bins (assumes this is of the LARGEST dataset)
     nbins = sc.histogram_bins(len(max(distributions, key=len)),bins)
-    print(nbins)
+    #print(nbins)
 
     if comp_distribution:
         # take samples from the equivalent theoretical distribution
@@ -83,16 +119,146 @@ def plot_histogram(data,bins='sqrt',comp_distribution=None,ax=None,labels=[]):
 
         # test the fit of the distribution using kolmogorov smirnov
         for dist_obs, lab in zip(distributions,labels):
-            ks_stat, pval, _, _ = scipy.stats.ks_2samp(dist_obs, theor_dist)
-            # ADD WHEN FORMATTING COMPLETED: annotate on graph
+            ks_stat, pval = scipy.stats.ks_2samp(dist_obs, theor_dist)
+
+            # formatting for p-value
+            if pval < 0.0001:
+                print_pval = '< 0.0001'
+            elif round(pval,3) == 0:
+                print_pval = round(pval,4)
+            else:
+                print_pval = round(pval,3)
+            
+            # print conclusion of ks test
+            if pval > ks_alpha:
+                print(f'Kolmogorov-Smirnov Test finds distribution to be {GREEN}statistically similar{END} to a {comp_distribution} distribution at a {ks_alpha} signifcance level')
+            else:
+                print(f'Kolmogorov-Smirnov Test finds distribution to be {RED}statistically different{END} to a {comp_distribution} distribution at a {ks_alpha} signifcance level')
+            
+            # get maximum of both distributions
+            ypos = max(*distributions)
+            xpos = nbins
+            ax.text(xpos,ypos,f'K-S Test Statistic: {round(ks_stat,3)}\nP-Value: {print_pval}')
         
         # append to the correct lists
         distributions.append(theor_dist)
         labels.append(comp_distribution)
 
+        # set title
+        ax.set_title(f'Histogram of Input Data with {comp_distribution} Overlay',loc='left') # REMINDER: ADD AUTOCAPS TO DIST
+    else:
+        ax.set_title('Histogram of Input Data',loc='left')
+
+    # format axes
     ax.hist(distributions,int(nbins),label=labels)
     ax.legend()
+    ax.set_title(f'{int(nbins)} Bins',loc='right')
+
+
+
+def generate_table(desired_stats,thresholds=None):
+    """
+    Function generates a nicely formatted table which 
+    summarises statistics contained in a dictionary. This 
+    can be categoriesed by "goodness" thresholds taken from
+    Prof. Liemohn's textbook if on.
+
+    Parameters
+    ----------
+    desired_stats : dict
+        Dictionary containing variable names and statistics
+        Should be ordered with the key as the label in the table
+        and the value as the variable.
+    thresholds : dict
+        Dictionary containing thresholds for statistics provided in
+        desired_stats. Note that the keys in each dictionary must match
+        or the function will fail.
+
+    Returns
+    -------
+    """
+    # determine the number of rows and columns in the table
+    rows = len(desired_stats) # get the length of the desired list of statistics
+    cols = len(desired_stats[0].keys())    # return the number of keys, should be the same for all entries
+
+    # create figure
+    fig, ax = plt.subplots(figsize=(rows*1.5,cols*1.8))
+
+    # set up the axis limits with "spacing" (a bit of padding on each side)
+    ax.set_ylim(-1, rows + 1)
+    ax.set_xlim(0, cols + .1)
+
+    # format the table
+    for row in range(rows):
+        ax.plot(
+            [0, cols + 1],
+            [row -.5, row - .5],
+            ls=':',
+            lw='.5',
+            c='grey'
+        )
     
+    # add header row differentiator
+    ax.plot([0, cols + 1], [rows-.5, rows-.5], lw='1', c='black')
+    ax.plot([0, cols + 1], [rows+.5, rows+.5], lw='1', c='black')
 
+    # hide axes
+    ax.axis('off')
 
+    # add nice differentiator shading for the row labels
+    rect = mpl.patches.Rectangle(
+        (0, -.5),  # bottom left starting position (x,y)
+        .95 * 1.5,  # width
+        rows,  # height
+        ec='none',
+        fc='grey',
+        alpha=.2,
+        zorder=-1
+    )
+    ax.add_patch(rect)
 
+    # loop over data to populate table in reverse order (x=0 is at the bottom)
+    for row,metric in enumerate(reversed(desired_stats)):
+        
+        # loop over columns to add to figure
+        for pos,key in enumerate(metric.keys()):
+            # check if is label for column
+            if key == 'metric':
+                ax.text(x=.1, y=row, s=metric[key],va='center', ha='left', weight='bold')
+            # check if threshold    
+            elif key == 'threshold':
+                ax.text(x=pos+.5, y=row, s=metric[key],va='center', ha='left', weight='bold')
+                # add dotted line after threshold
+                ax.plot([pos+1.32,pos+1.32], [-0.5,rows-.5], ls=':', lw=.5, c='lightgrey',zorder=1)
+
+            if thresholds != None:
+                # remember, order of variables is (score, success rate)
+                #check for NaNs
+                if np.isnan(metric[key][0]):
+                    color = 'k'
+                # check if the value is good, bad or okay
+                elif metric[key][1] == 'good':
+                    color = grass
+
+                elif metric[key][1] == 'bad':
+                    color = mauve
+
+                else:
+                    color = 'k'
+                    
+                # now plot in the correct color
+                ax.text(x=pos+.5, y=row, s=round(metric[key][0],2),va='center', ha='left', weight='bold',c=color)
+
+    # add title to table
+    #ax.set_title(f"Event Detection Metrics Summary Table {subset_method}",fontsize=16,fontweight='bold')
+
+    # spoof a legend below the title to explain the color codes
+    custom_lines = [Line2D([0], [0], color=grass, lw=4),
+                    Line2D([0], [0], color=mauve, lw=4),
+                    Line2D([0], [0], color='k', lw=4)]
+    
+    ax.legend(custom_lines, 
+              ['Good Performance', 'Poor Performance'], 
+              ncol=2,
+              loc='center', bbox_to_anchor=(0.5,0.986),
+              frameon=False)
--- geometrics/stats_calcs.py
+++ geometrics/stats_calcs.py
@@ -12,6 +12,28 @@ import warnings
 import numpy as np
 import scipy
 
+#############
+## GLOBALS ##
+#############
+
+# Dictionary to map distribution names to their corresponding scipy.stats objects
+common_distributions = {
+    'normal': scipy.stats.norm,
+    'lognormal' : scipy.stats.lognorm,
+    'exponential': scipy.stats.expon,
+    'logistic': scipy.stats.logistic,
+    'powerlaw' : scipy.stats.powerlaw,
+    'pareto': scipy.stats.pareto,
+    'genpareto': scipy.stats.genpareto,
+    'weibull min': scipy.stats.weibull_min,
+    'weibull max': scipy.stats.weibull_max,
+    'gumbel right': scipy.stats.gumbel_r,
+    'gumbel left': scipy.stats.gumbel_l,
+    'inverse normal' : scipy.stats.invgauss,
+    'gamma' : scipy.stats.gamma
+}
+
+#########################################################################################################################
 
 def histogram_bins(length, rule='sqrt') -> int:
     """
@@ -49,7 +71,7 @@ def histogram_bins(length, rule='sqrt') -> int:
     return nbins
 
 
-def get_theoretical_dist(data,comp_distribution):
+def get_theoretical_dist(data,comp_distribution,distributions=common_distributions):
     """
     Function gets a theoretical distribution of a given type
     and returns a fit, an equivalent sampled distribution or
@@ -66,11 +88,11 @@ def get_theoretical_dist(data,comp_distribution):
         'powerlaw',
         'pareto',
         'genpareto',
-        'weibull_min',
-        'weibull_max',
-        'gumbel_r',
-        'gumbel_l',
-        'inv_norm',
+        'weibull min',
+        'weibull max',
+        'gumbel right',
+        'gumbel left',
+        'inverse normal',
         'gamma'
 
     Parameters
@@ -79,6 +101,9 @@ def get_theoretical_dist(data,comp_distribution):
         array with the data to fit similar distribution to
     comp_distribution : str
         string defining the distribution type
+    distributions : dict
+        dictionary containing key value pairs of the
+        distributions common in scipy
 
     Returns
     -------
@@ -87,22 +112,6 @@ def get_theoretical_dist(data,comp_distribution):
     pdf_fitted : type
         fitted model from the theoretical distribution
     """
-    # Dictionary to map distribution names to their corresponding scipy.stats objects
-    distributions = {
-        'normal': scipy.stats.norm,
-        'lognormal' : scipy.stats.lognorm,
-        'exponential': scipy.stats.expon,
-        'logistic': scipy.stats.logistic,
-        'powerlaw' : scipy.stats.powerlaw,
-        'pareto': scipy.stats.pareto,
-        'genpareto': scipy.stats.genpareto,
-        'weibull_min': scipy.stats.weibull_min,
-        'weibull_max': scipy.stats.weibull_max,
-        'gumbel_r': scipy.stats.gumbel_r,
-        'gumbel_l': scipy.stats.gumbel_l,
-        'inv_norm' : scipy.stats.invgauss,
-        'gamma' : scipy.stats.gamma
-    }
     
     dist = distributions.get(comp_distribution.lower())
     if dist is not None:
@@ -123,4 +132,84 @@ def get_theoretical_dist(data,comp_distribution):
                 print(f"Error fitting {comp_distribution} distribution: {e}")
     
     return comp_samples, pdf_fitted
-                
+
+def find_best_fit(data,distributions=common_distributions,gen_samples=False,alpha=0.05):
+    """
+    Function tries to find the ideal fit for a distribution
+    from a number of theoretical distributions common in the
+    geosciences.
+
+    The simple version of this function uses ONLY the Kolmogorov-Smirnov
+    test, but a more complex set of tests will be added later also using
+    the Cramer Von-Misses test and Anderson-Darling test.
+
+    note: updates to this function should allow for a user supplied
+    distribution
+
+    Parameters
+    ----------
+    data : arrayLike
+        array containing the data to be fitted to
+    distributions : dict
+        dictionary containing key value pairs of the
+        distributions common in scipy
+    gen_samples : bool
+        specify whether to fit the best theoretical distribution
+        and return an equally sized sample
+    alpha : float
+        significance level to solve at
+    
+    Returns
+    -------
+    dist_results : dict
+        dictionary with each distribution and their P-Value
+    best_theor_dist : arrayLike
+        best theoretical distribution for the data
+    """
+    # set dictionary to store results
+    dist_results = {}
+    # check if there is a better way for this to be done
+    best_stat = None
+    best_pval = None
+    best_dist = None
+    
+
+    # loop through the key value pairs in the distribution and test
+    for dist_name, val in zip(distributions.keys(),distributions.values()):
+        # fit a distribution
+        samples, theor_dist = get_theoretical_dist(data,dist_name)
+        # perform K-S test with sampled distribution
+        ks_stat, pval = scipy.stats.ks_2samp(data, samples)
+        ks_stat = round(ks_stat,2)
+        # store results in dictionary
+        dist_results[dist_name] = (ks_stat,pval)
+
+        # if there is no best distribution, store the results immediately
+        if best_dist is None:
+            best_dist = dist_name
+            best_stat = ks_stat
+            best_pval = pval
+        
+        # if the best P-Value is not statistically significant
+        elif best_pval > alpha:
+            # replace if best the test statistic is larger the current one
+            if best_stat > ks_stat:
+                best_stat = ks_stat
+                best_dist = dist_name
+                best_pval = pval
+
+        # if the best P-Value is statistically significant    
+        else:
+            # replace if the current data is statistically significant AND 
+            # the test statistic is smaller than the best test statistic so far.
+            if (best_pval < alpha and best_stat > ks_stat):
+                best_stat = ks_stat
+                best_dist = dist_name
+                best_pval = pval
+
+    print(f'Best Distribution Fit: {best_dist}')
+
+    if gen_samples:
+        return samples, dist_results
+    else:
+        return dist_results
